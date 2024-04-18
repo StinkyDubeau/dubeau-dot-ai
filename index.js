7cd7822327a28880b809e9f1ce3d185ed984ea70
StinkyDubeau <jakersunderscore09@gmail.com>
2024-04-18 18:29:49 -0400

    Remove junk data

--- index.js
+++ index.js
@@ -17,27 +17,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const astros = [
-    // {
-    //     image: "https://cdn.discordapp.com/attachments/937152144170164286/1222245255961645236/IMG_4819.jpg?ex=66313319&is=661ebe19&hm=a09bfe70fefa7bcdcc88f9e6a3e8569cab55cb188b76d605fc382379eddd9a71&",
-    //     description:
-    //         "This is a text description that will be from 100 to 200 words of the astro.",
-    //     title: "Astro in the woods",
-    // },
-    // {
-    //     image: "https://media.discordapp.net/attachments/937152144170164286/1221462776636248118/IMG_4796.jpg?ex=662e5a5b&is=661be55b&hm=89b0c6bf836d4ad79550dd519d21ca6913213b6cf6a3d247a843c72c191d286c&=&format=webp&width=265&height=354",
-    //     description:
-    //         "This is a text description that will be from 100 to 200 words of the astro.",
-    //     title: "Astro approach",
-    // },
-    // {
-    //     image: "https://media.discordapp.net/attachments/937152144170164286/1218726850948370452/IMG_6605.png?ex=662da0d4&is=661b2bd4&hm=25fca5c4f629fdc74b93f36dbbc326a25d6b8be3d2d36e7d55023635438117cd&=&format=webp&quality=lossless&width=162&height=350",
-    //     description:
-    //         "This is a text description that will be from 100 to 200 words of the astro.",
-    //     title: "Google maps astro",
-    // },
+
 ];
 
 app.get("/astros", (req, res) => {
+	res.send(astros);
+});
+
+app.get("/ask", (req, res) => {
 	res.send(astros);
 });
 
@@ -54,9 +41,9 @@ function loadAstros() {// Create a new client instance
 	// When the client is ready, run this code (only once).
 	// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 	// It makes some properties non-nullable.
-	client.once(Events.ClientReady, readyClient => {
-		console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-	});
+	// client.once(Events.ClientReady, readyClient => {
+	// 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	// });
 
     client.on('ready', async() => {
         console.log(`Logged in as ${client.user.tag}`);
