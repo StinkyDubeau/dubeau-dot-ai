00c5fb3e3a52758986afcfab81709da0c0e9a85a
StinkyDubeau <jakersunderscore09@gmail.com>
2024-04-18 12:19:09 -0400

    Add discord.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}.`);
+    console.log(`Server running on port ${port}.`);
 });
 
 async function ask(prompt) {
