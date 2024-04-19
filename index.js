4248fe52ad73b49f6799161c1c223e79ae04857c
StinkyDubeau <jakersunderscore09@gmail.com>
2024-04-18 20:47:03 -0400

    Work on image downloader

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import cors from "cors";
 import 'dotenv/config';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import bodyParser from "body-parser";
+import download from "image-downloader";
+import path from "node:path";
 
-const model = "nous-hermes2";
+const model = "llava:13b";
 const port = process.env.PORT;
 const channelId = process.env.CHANNEL_ID;
 
@@ -23,19 +25,29 @@ app.get("/astros", (req, res) => {
 });
 
 app.post("/ask", async (req, res, next) => {
-    console.log(req.body);
-    res.sendStatus(200);
+    console.log("Asking...");
 
-    res = await ollama.chat({
-        model: 'llava',
-        messages: [{
-            role: 'user',
-            content: 'Describe this image:',
-            images: [req.body.image]
-        }]
-    })
-	// res.send(await ask("Describe the Chevrolet Astro van."));
+    res.send(await describe(req.body.image));
 });
+
+async function describe(url) {
+    console.log("Downloading image...");
+    const image = await downloadImage(url);
+
+    console.log("Generating response...");
+    const response = await ollama.chat({
+        model: model,
+        messages: [
+            {
+                role: "user",
+                content: "Describe this image:",
+                image: image
+            }
+        ]
+    });
+    return response;
+}
+
 
 app.get("/", (req, res) => {
 	res.send("<p>Hello world</p>");
@@ -45,6 +57,13 @@ app.listen(port, () => {
     loadAstros();
     console.log(`Server running on port ${port}.`);
 });
+
+async function downloadImage(url) {
+    return await download.image({
+       url,
+       dest: path.dirname("/img") 
+    });
+}
 
 function loadAstros() {// Create a new client instance
 	// When the client is ready, run this code (only once).
@@ -92,18 +111,6 @@ function loadAstros() {// Create a new client instance
 	client.login(process.env.DISCORD_TOKEN);
 }
 
-async function ask(prompt) {
-    const response = await ollama.chat({
-        model: model,
-        messages: [
-            {
-                role: "user",
-                content: prompt,
-            }
-        ]
-    });
-    return response;
-}
+
 
 console.log("App running.");
-// console.log(ask("What's the time in Jakarta?"));
